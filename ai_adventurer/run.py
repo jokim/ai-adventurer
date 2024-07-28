0001d6841ac438603b9ed85ebd7867c89ca60019
Joakim Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2024-07-28 14:30:38 +0200

    Temp changes while development, not working

--- ai_adventurer/run.py
+++ ai_adventurer/run.py
@@ -18,13 +18,12 @@ class Game(object):
         # The state of where you are in the GUI. A bit too simple, probably.
         self.status = None
         self.gui = cli_gui.GUI()
-        self.nlp = nlp.NLP()
 
         self.lines = []
 
         self.game_actions = {
-            'j': ("Up", self.shift_focus_up),
-            'k': ("Down", self.shift_focus_down),
+            'j': ("Down", self.shift_focus_down),
+            'k': ("Up", self.shift_focus_up),
             'r': ("Retry", self.retry),
             'e': ("Edit", self.edit_line),
             'q': ("Quit", self.quit_game),
@@ -33,12 +32,15 @@ class Game(object):
 
 
     def run(self):
-        with self.gui.fullscreen():
+        with self.gui.activate():
             self.gui.show_splashscreen()
             self.start_mainmenu_loop()
 
 
     def start_mainmenu_loop(self):
+        # TODO: HACK FOR JUST STARTING A NEW GAME WHILE CODING
+        self.start_new_game()
+        # TODO: HACK FOR JUST STARTING A NEW GAME WHILE CODING
         choices = {
             'n': ('New game', self.start_new_game),
             'q': ('Quit', self.quit),
@@ -60,14 +62,31 @@ class Game(object):
 
     def start_new_game(self):
         self.lines = []
+        #self.nlp_thread = nlp.OpenAINLPThread()
+        #self.nlp_thread = nlp.LocalNLPThread()
+        self.nlp_thread = nlp.GeminiNLPThread()
+        #self.nlp_thread = nlp.MockNLPThread()
         print("New game!")
 
         self.gui.start_gameroom(choices=self.game_actions, lines=[],
                                 status="New game, generating...")
 
-        # TODO: put text generation into its own thread?
+        # TODO: These should be per game later
+        instructions = """
+            You are a very good story writer assistant, helping with creating
+            an adventure by the given instructions. Return one sentence,
+            continuing the given story.
+            """
+        # TODO: Temp start, just to have something
+        story_line = """
+            This is a cyberpunk story, in the year 2345. You are called Lou,
+            and are an android, living in the metropoly of New Zhu.
+            One day you woke up, and started thinking
 
-        initial_text = self.nlp.generate()
+            """
+
+        self.lines.append(story_line)
+        initial_text = self.nlp_thread.prompt(instructions + "\n\n" + story_line)
         self.lines.append(initial_text)
 
         # Avoid duplicating all the text... Have its own object for it?
@@ -95,30 +114,37 @@ class Game(object):
 
     def next_line(self):
         """Generate new text"""
-        self.gui.send_message("Generate more text...")
-        text = self.nlp.generate(self.lines)
+        self.gui.send_message("Generating more text...")
+        text = self.nlp_thread.prompt("Give another sentence", role='system')
         self.lines.append(text)
         self.gui._lines = self.lines
         self.gui.print_screen("New text generated")
 
 
     def shift_focus_up(self):
-        pass
+        self.gui.set_focus_up()
+        self.gui.print_screen()
 
     def shift_focus_down(self):
-        pass
+        self.gui.set_focus_down()
+        self.gui.print_screen()
 
     def retry(self):
         """Retry last generation"""
         self.gui.send_message("Retry last text")
-        text = self.nlp.generate(self.lines[:-1])
+        # TODO: remove last line
+        text = self.nlp_thread.prompt('', role='system')
         self.lines[-1] = text
         self.gui._lines = self.lines
         self.gui.print_screen("New text generated")
 
 
     def edit_line(self):
-        pass
+        """Edit last line/response"""
+        newline = self.gui.edit_last_line()
+        self.lines[-1] = newline
+        self.gui._lines = self.lines
+        self.gui.print_screen('Last line updated')
 
 
     def quit_game(self):
