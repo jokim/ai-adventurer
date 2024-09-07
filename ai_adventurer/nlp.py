48f15d803401acd849c91f121ec389ab75c0bbb1
Joakim Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2024-09-07 21:03:52 +0200

    Add support for overriding max tokens returned

--- ai_adventurer/nlp.py
+++ ai_adventurer/nlp.py
@@ -29,11 +29,14 @@ class NLPClient(object):
 
     """
 
+    # Default max tokens to return from the NLP in prompts
+    max_tokens = 100  # about 25-50 words?
+
     def __init__(self, secrets=None, extra=None, modelname=None):
         self.secrets = secrets
         self.modelname = modelname
 
-    def prompt(self, text=None, instructions=None):
+    def prompt(self, text=None, instructions=None, max_tokens=None):
         """Subclass for the specifig NLP generation.
 
         Adds the previous dialog to the prompt, for giving context.
@@ -61,7 +64,7 @@ class MockNLPClient(NLPClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def prompt(self, text=None, instructions=None):
+    def prompt(self, text=None, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         import random
 
@@ -84,26 +87,27 @@ class LocalNLPClient(NLPClient):
         model = keras.saving.load_model(keras_file, compile=True)
         return model
 
-    def prompt(self, text, instructions=None):
+    def prompt(self, text, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         text = self.convert_to_prompt(text, role="user")
         if instructions:
             text = instructions + " " + text
 
-        output = self._generate(text)
+        output = self._generate(text, max_tokens=max_tokens)
 
         # Remove the pretext, so only the new ouptut is returned
         output = output[len(text):].strip()
         logger.debug("Prompt response: %s", output)
         return output
 
-    def _generate(self, pretext):
+    def _generate(self, pretext, max_tokens=None):
         logger.debug("Generating with prompt: '%s'", pretext)
         logger.debug("Prompt length: %s", len(pretext))
 
         start = time.time()
         output = self.model.generate(
-            pretext, max_length=min(1024, len(pretext) + 40)
+            pretext, max_length=min(1024, len(pretext) + (max_tokens or
+                                                          self.max_tokens))
         ).strip()
         end = time.time()
 
@@ -128,21 +132,21 @@ class HuggingfaceNLPClient(NLPClient):
                         model_kwargs={"torch_dtype": torch.bfloat16},
                         device_map="auto")
 
-    def prompt(self, text, instructions=None):
+    def prompt(self, text, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         text = self.convert_to_prompt(text, role="user")
         if instructions:
             text = instructions + " " + text
-        output = self._generate(text)
+        output = self._generate(text, max_tokens=max_tokens)
         return output
 
-    def _generate(self, pretext):
+    def _generate(self, pretext, max_tokens=None):
         logger.debug("Generating with prompt: '%s'", pretext)
         logger.debug("Prompt length: %s", len(pretext))
         start = time.time()
         raw = self.model(pretext,
                          return_full_text=False,
-                         max_new_tokens=30)
+                         max_new_tokens=max_tokens or self.max_tokens)
         output = raw[0]["generated_text"]
         end = time.time()
 
@@ -224,7 +228,7 @@ class MockOnlineNLPClient(OnlineNLPClient):
             self.modelname = self.mock_model
         self.apikey = self._get_api_key()
 
-    def prompt(self, text=None, instructions=None):
+    def prompt(self, text=None, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         import random
 
@@ -245,9 +249,6 @@ class OpenAINLPClient(OnlineNLPClient):
     api_key_url = "https://platform.openai.com/api-keys"
     secrets_api_key_name = "openai-key"
 
-    # Max tokens to return from the AI in prompts
-    max_tokens = 400  # about 100-200 words?
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         from openai import OpenAI
@@ -256,7 +257,7 @@ class OpenAINLPClient(OnlineNLPClient):
             self.modelname = self.openai_model
         logger.debug(f"Model: {self.modelname}")
 
-    def prompt(self, text, instructions=None):
+    def prompt(self, text, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         # Reformat the prompt to follow OpenAIs specs:
         new_text = []
@@ -269,7 +270,7 @@ class OpenAINLPClient(OnlineNLPClient):
         answer = self.client.chat.completions.create(
             model=self.modelname,
             messages=new_text,
-            max_tokens=self.max_tokens,
+            max_tokens=max_tokens or self.max_tokens,
             n=1,
             stream=False,
         )
@@ -287,9 +288,35 @@ class GeminiNLPClient(OnlineNLPClient):
     """
 
     google_model = "gemini-1.5-flash"
-
     api_key_url = "https://aistudio.google.com/app/apikey"
     secrets_api_key_name = "gemini-key"
+
+    # The sampling temperature, between 0.0 and 1.0.
+    # From <https://docs.mistral.ai/api/#tag/chat>:
+    # Higher values like 0.8 will make the output more random, while lower
+    # values like 0.2 will make it more focused and deterministic. We generally
+    # recommend altering this or top_p but not both.
+
+    # Control the randomness of the output. Note: The default value varies by
+    # model, see the `Model.temperature` attribute of the `Model` returned the
+    # `genai.get_model` function.
+    # Values can range from [0.0,1.0], inclusive. A value closer to 1.0 will
+    # produce responses that are more varied and creative, while a value closer
+    # to 0.0 will typically result in more straightforward responses from the
+    # model.
+    temperature = 0.75
+
+    # Maximum cumulative probability of tokens to consider when sampling.
+    # The Gemini model uses combined Top-k and nucleus sampling.
+    #
+    # Tokens are sorted based on their assigned probabilities so that only the
+    # most likely tokens are considered. Top-k sampling directly limits the
+    # maximum number of tokens to consider, while Nucleus sampling limits
+    # number of tokens based on the cumulative probability.
+    #
+    # Note: The default value varies by model, see the `Model.top_p` attribute
+    # of the `Model` returned the `genai.get_model` function.
+    top_p = 1.0
 
     # Google's tresholds are very sensitive, so need to adjust these. Keep low
     # for now.
@@ -312,13 +339,20 @@ class GeminiNLPClient(OnlineNLPClient):
         genai.configure(api_key=self._get_api_key())
         logger.debug(f"Model: {self.modelname}")
 
-    def prompt(self, text, instructions=None):
+    def prompt(self, text, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         import google.generativeai as genai
+        generation_config = genai.GenerationConfig(
+            candidate_count=1,
+            max_output_tokens=max_tokens or self.max_tokens,
+            temperature=self.temperature,
+            top_p=self.top_p,
+        )
         client = genai.GenerativeModel(
             self.modelname,
             safety_settings=self.safety_settings,
             system_instruction=instructions,
+            generation_config=generation_config,
         )
         starttime = time.time()
         response = client.generate_content(
@@ -380,9 +414,6 @@ class MistralNLP(OnlineNLPClient):
     # Waiting limit when waiting for response from the AIs API
     timeout_ms = 120000
 
-    # Max tokens to return from the AI in prompts
-    max_tokens = 100  # about 25-50 words?
-
     # The sampling temperature, between 0.0 and 1.0.
     # From <https://docs.mistral.ai/api/#tag/chat>:
     # Higher values like 0.8 will make the output more random, while lower
@@ -405,7 +436,7 @@ class MistralNLP(OnlineNLPClient):
         if not self.modelname:
             self.modelname = self.mistral_model
 
-    def prompt(self, text, instructions=None):
+    def prompt(self, text, instructions=None, max_tokens=None):
         super().prompt(text, instructions)
         # Reformat the prompt to follow Mistrals specs:
         new_text = []
@@ -416,7 +447,7 @@ class MistralNLP(OnlineNLPClient):
 
         import mistralai
         try:
-            response = self._prompt(new_text)
+            response = self._prompt(new_text, max_tokens=max_tokens)
         except mistralai.models.sdkerror.SDKError as e:
             if e.status_code == 401:
                 raise NotAuthenticatedError(e)
@@ -437,12 +468,12 @@ class MistralNLP(OnlineNLPClient):
         logger.debug("Prompt response: %s", answer)
         return answer
 
-    def _prompt(self, text):
+    def _prompt(self, text, max_tokens=None):
         starttime = time.time()
         response = self.client.chat.complete(
             model=self.modelname,
             messages=text,
-            max_tokens=self.max_tokens,
+            max_tokens=max_tokens or self.max_tokens,
             timeout_ms=self.timeout_ms,
             safe_prompt=False,
             stream=False,
@@ -563,7 +594,8 @@ class NLPHandler(object):
                 ret.append(line)
         return "\n".join(ret)
 
-    def prompt(self, text, instructions=None, return_raw=False):
+    def prompt(self, text, instructions=None, return_raw=False,
+               max_tokens=None):
         """Ask the NLP and return the result"""
         # TODO: handle the text in various formats
         if instructions is None:
@@ -572,7 +604,8 @@ class NLPHandler(object):
         response = self.nlp_client.prompt(
             # TODO: use self.remove_internal_comments on text input too?
             text=self.clean_text(text),
-            instructions=self.remove_internal_comments(instructions)
+            instructions=self.remove_internal_comments(instructions),
+            max_tokens=max_tokens,
         )
         if return_raw:
             return response
@@ -584,7 +617,7 @@ class NLPHandler(object):
             """Give me 100-200 words describing an idea for one exiting fantasy
             story. Only return one story. Return a summary of the story,
             including a chapter layout and character descriptions. Do not start
-            the story.""")
+            the story.""", max_tokens=800)
         # TODO; might change this depending on what AI model to use?
 
         # TODO: Make use of the APIs possibility to fill a object with the
@@ -598,7 +631,7 @@ class NLPHandler(object):
             f"""Give me only one title, max 40 characters, for a story with
             the given concept, without any other feedback and no newlines:
             {concept_or_summary}
-            """)
+            """, max_tokens=20)
         # TODO; might change this depending on what AI model to use
 
         # Some models respond weirdly to this, with a lot of newlines.
